fcf474a0b3c1e32ae54b7a6cc2617a23ecbee741
Prasanth-Shanmugam <prasanthshanmugam1402@gmail.com>
2020-08-21 02:28:53 +0530

    Added: playing till position reaches 100

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -2,22 +2,24 @@ import java.util.Random;
 class SnakeAndLadder{
    private static Random random = new Random();
 	public static void main(String[] args){
-		int playerPosition = 10;
-		int dieOutput = randomGenerator(6);
-         System.out.println(playerPosition+" is the position and die gives "+dieOutput);
-		int snakeLadderPositions = randomGenerator(3);
-		if(snakeLadderPositions==3){
-			playerPosition -= dieOutput;
-			System.out.println("HISSSSSSS"); 
+		int playerPosition = 0;
+		while(playerPosition<100){
+			int dieOutput = randomGenerator(6);
+        	System.out.println(playerPosition+" is the position and die gives "+dieOutput);
+			int snakeLadderPositions = randomGenerator(3);
+			if(snakeLadderPositions==3){
+				playerPosition -= dieOutput;
+				System.out.println("HISSSSSSS"); 
+				}
+			else if(snakeLadderPositions==2){
+      	   playerPosition += dieOutput;
+				System.out.println("TADAAAAAAA !! Lets Climb");
 			}
-		else if(snakeLadderPositions==2){
-         playerPosition += dieOutput;
-			System.out.println("TADAAAAAAA !! Lets Climb");
+			else{
+				System.out.println("Staying at the same Position safely!");
+			}
+				System.out.println("Now player is at : "+playerPosition);
 		}
-		else{
-			System.out.println("Staying at the same Position safely!");
-		}
-			System.out.println(playerPosition);
 	}
 	private static int randomGenerator(int n){
 		int result  = random.nextInt(n)+1;	
