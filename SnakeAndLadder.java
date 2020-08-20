d6683a85637c7f2c9d29126c3d7b4dd741518461
Prasanth-Shanmugam <prasanthshanmugam1402@gmail.com>
2020-08-21 02:44:28 +0530

    Added : Total number of Die Rolls and player position for each roll

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -2,9 +2,10 @@ import java.util.Random;
 class SnakeAndLadder{
    private static Random random = new Random();
 	public static void main(String[] args){
-		int playerPosition = 0;
+		int playerPosition = 0,dieRollCount=0;
 		while(playerPosition<100){
 			int dieOutput = randomGenerator(6);
+			dieRollCount++;
         	System.out.println(playerPosition+" is the position and die gives "+dieOutput);
 			int snakeLadderPositions = randomGenerator(3);
 			if(snakeLadderPositions==3){
@@ -24,6 +25,7 @@ class SnakeAndLadder{
 			}
 			System.out.println("Now player is at : "+playerPosition);
 		}
+      System.out.println("Dice Rolled for  : "+dieRollCount+" times ");		
 	}
 	private static int randomGenerator(int n){
 		int result  = random.nextInt(n)+1;	
