9cbe7d2d31c46a95960a12bbcd53a59326cfc44c
Prasanth-Shanmugam <prasanthshanmugam1402@gmail.com>
2020-08-21 01:50:11 +0530

    Added: Die Roller

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,5 +1,9 @@
+import java.util.Random;
 class SnakeAndLadder{
 	public static void main(String[] args){
 		int playerPosition = 0;
+		Random random = new Random();
+		int dieRoll  = random.nextInt(6)+1;
+		System.out.println(dieRoll);	
 	}
 }
