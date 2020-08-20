a43ecd507f9506fd9321f9e26e11783c6ce125b8
Prasanth-Shanmugam <prasanthshanmugam1402@gmail.com>
2020-08-21 02:15:58 +0530

    Added: snakes, ladders and no play

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,9 +1,26 @@
 import java.util.Random;
 class SnakeAndLadder{
+   private static Random random = new Random();
 	public static void main(String[] args){
-		int playerPosition = 0;
-		Random random = new Random();
-		int dieRoll  = random.nextInt(6)+1;
-		System.out.println(dieRoll);	
+		int playerPosition = 10;
+		int dieOutput = randomGenerator(6);
+         System.out.println(playerPosition+" is the position and die gives "+dieOutput);
+		int snakeLadderPositions = randomGenerator(3);
+		if(snakeLadderPositions==3){
+			playerPosition -= dieOutput;
+			System.out.println("HISSSSSSS"); 
+			}
+		else if(snakeLadderPositions==2){
+         playerPosition += dieOutput;
+			System.out.println("TADAAAAAAA !! Lets Climb");
+		}
+		else{
+			System.out.println("Staying at the same Position safely!");
+		}
+			System.out.println(playerPosition);
+	}
+	private static int randomGenerator(int n){
+		int result  = random.nextInt(n)+1;	
+		return result;
 	}
 }
